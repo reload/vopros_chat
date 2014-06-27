a1544be52ce8830cd9d7ca1e146297d2f44227b7
Arne Jørgensen <arne@reload.dk>
2014-06-27 14:43:31 +0200

    Fixed data escaping and indentation.

--- templates/vopros-chat-log-activity-feed-items.tpl.php
+++ templates/vopros-chat-log-activity-feed-items.tpl.php
@@ -6,10 +6,10 @@
 ?>
 <?php foreach ($variables['log_items']['#log'] as $timestamp => $item): ?>
 <div class="vopros-chat-message">
-<div class="message-content">
-   <span class="message-author"><?php echo $item->name ?>: </span>
-   <span class="message-text"><?php echo $item->msg ?></span>
-   </div>
+  <div class="message-content">
+    <span class="message-author"><?php echo $item->name ?>: </span>
+    <span class="message-text"><?php echo $item->msg ?></span>
+  </div>
   <span class="message-time"><?php echo format_date($timestamp, 'custom', 'H:i') ?></span>
 </div>
 <?php endforeach; ?>
--- templates/vopros-chat-log-activity-feed.tpl.php
+++ templates/vopros-chat-log-activity-feed.tpl.php
@@ -5,11 +5,11 @@
  */
 ?>
 <div class="log-feed-item feed-item">
-  <div class="log-header"><?php echo t('Chat with !user_name on !date', array(
-               '!user_name' => $variables['log']['#question']->user_name,
+  <div class="log-header"><?php echo t('Chat with @user_name on !date', array(
+               '@user_name' => $variables['log']['#question']->user_name,
                '!date' => format_date($variables['log']['#question']->created, 'short'),
              )) ?></div>
   <div class="log-content vopros-chat">
-     <?php echo render($variables['log']['#chatlog']) ?>
+    <?php echo render($variables['log']['#chatlog']) ?>
   </div>
 </div>
