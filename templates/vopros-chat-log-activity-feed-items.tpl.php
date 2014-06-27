6aaaa2e1a66c9dc3e098bfd3cd9500ea265ae248
Arne Jørgensen <arne@reload.dk>
2014-06-27 13:49:51 +0200

    Added chat log in question activity feed.

---
+++ templates/vopros-chat-log-activity-feed-items.tpl.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file
+ * Render chat log items for an activity feed item.
+ */
+?>
+<?php foreach ($variables['log_items']['#log'] as $timestamp => $item): ?>
+<div class="vopros-chat-message">
+<div class="message-content">
+   <span class="message-author"><?php echo $item->name ?>: </span>
+   <span class="message-text"><?php echo $item->msg ?></span>
+   </div>
+  <span class="message-time"><?php echo format_date($timestamp, 'custom', 'H:i') ?></span>
+</div>
+<?php endforeach; ?>
