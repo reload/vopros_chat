c75d21ab36d966956d12c28523f8cfb28d19ec29
Thomas Fini Hansen <xen@xen.dk>
2014-08-22 20:48:14 +0200

    Don't index log arrays on timestamp. Might not be unique.

--- templates/vopros-chat-log-activity-feed-items.tpl.php
+++ templates/vopros-chat-log-activity-feed-items.tpl.php
@@ -5,12 +5,12 @@
  */
 global $user;
 ?>
-<?php foreach ($variables['log_items']['#log'] as $timestamp => $item): ?>
+<?php foreach ($variables['log_items']['#log'] as $item): ?>
 <div class="vopros-chat-message">
   <div class="message-content">
     <span class="message-author<?php echo ($user->uid == $item->uid) ? ' message-author-me' : '' ?>"><?php echo ($user->uid == $item->uid) ? t('Me') : $item->name ?>: </span>
     <span class="message-text"><?php echo $item->msg ?></span>
   </div>
-  <span class="message-time"><?php echo format_date($timestamp, 'custom', 'G:i') ?></span>
+  <span class="message-time"><?php echo format_date($item->timestamp, 'custom', 'G:i') ?></span>
 </div>
 <?php endforeach; ?>
