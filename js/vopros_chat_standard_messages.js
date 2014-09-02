c6f233bf041c65da7ae72f377f4b2ccfc8f8cd5d
Thomas Fini Hansen <xen@xen.dk>
2014-09-02 13:34:51 +0200

    Styled standard messages. Ref VOP-546.

--- js/vopros_chat_standard_messages.js
+++ js/vopros_chat_standard_messages.js
@@ -2,20 +2,22 @@
 (function ($) {
   Drupal.behaviors.voprosChatStandardMessages = {
     attach: function (context, settings) {
-      $('.standard-message').once('standard-message', function() {
+      $('.standard-group').once('standard-group', function() {
         var content = $('> .content', $(this));
         content.hide();
         $('> .title', $(this)).click(function (e) {
           e.preventDefault();
-          content.slideToggle();
+          content.slideToggle('fast');
         });
+      });
+      $('.standard-message').once('standard-message', function() {
         $('> .message', $(this)).click(function (e) {
           e.preventDefault();
           var chat = $(this).parents('.vopros-chat').get(0);
           var textarea = $(chat).find('textarea');
           textarea.val(textarea.val() + $(this).nextAll('.text').text());
           // Hide messages again.
-          $('.standard-message > .content', chat).hide();
+          $('.standard-group > .content', chat).slideUp('fast');
         });
       });
     }
